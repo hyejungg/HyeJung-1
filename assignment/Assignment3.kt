73196258dfd113fd1fe2014105adc13d17efa4a5
hyejungg <kimhyejung12@naver.com>
2022-04-17 23:10:01 +0900

    [chore] 하나의 함수는 한가지 동작만 하도록 수정

--- assignment/Assignment3.kt
+++ assignment/Assignment3.kt
@@ -5,21 +5,22 @@ data class Food(
     val price: Int
 )
 
-fun printFoodList(foodList: List<Food>, foodTotalCount: Int) {
+fun printFoodList(foodList: List<Food>) {
     println("<출력>")
     foodList.forEach {
         println("${it.name} ${it.price}원")
     }
-    println("------------------")
-    println("총합 ${foodTotalCount}원")
 }
 
-fun main(){
+fun printFoodTotalCount(foodTotalCount: Int) = println("------------------\n총합 ${foodTotalCount}원")
+
+fun main() {
     val foodList = listOf<Food>(
         Food("돼지목살", 6000),
         Food("제로콜라", 2000),
         Food("민트초코오레오", 1500),
         Food("진라면순한맛", 1000)
     )
-    printFoodList(foodList, foodList.sumOf { it.price })
+    printFoodList(foodList)
+    printFoodTotalCount(foodList.sumOf { it.price })
 }
